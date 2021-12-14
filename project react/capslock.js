ff9aa1a3276433da162fe63bd19c34ea1b37c4d6
Lucas Amancio <74212108+lucasskt18@users.noreply.github.com>
2021-12-14 15:55:41 -0300

    Add files via upload

---
+++ project react/capslock.js
@@ -0,0 +1,10 @@
+<CapsLock texto="Me deixe em caps Lock por favor"/>
+
+function CapsLock (props) {
+   const textoInserido = props.texto
+   const textoEmCapsLock = textoInserido.toUpperCase()
+   return <div>(textoEmCapsLock</div>
+
+
+
+}
